ae90b14289f954d4902f9eab0d2c0d344f5b82f9
zhangqiao <1053373312@qq.com>
2019-09-27 14:16:51 +0800

    修改登录样式 添加退出按钮

--- frontend/src/components/index/PageHeader.vue
+++ frontend/src/components/index/PageHeader.vue
@@ -1,18 +1,47 @@
 <template>
-
-  <div class="left">
-    <img src="../../assets/header_logo.png" alt="">
+  <div>
+    <div class="left">
+      <img src="../../assets/header_logo.png" alt="">
+    </div>
+    <div class="right">
+      <el-dropdown :hide-on-click="false">
+        <span class="el-dropdown-link">
+          <i class="el-icon-user-solid"></i><i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item command="a">
+            <span @click="logout" style="display:block;">退出</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: "Header"
+        name: "Header",
+        methods:{
+            logout(){
+                console.log("退出");
+                this.$router.push({path:'/'})
+            }
+        }
     }
 </script>
 
 <style scoped>
 .left{
   float: left;
+}
+.right{
+    float: right;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409EFF;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
 }
 </style>
--- frontend/src/views/login/login.vue
+++ frontend/src/views/login/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form ref="form" :model="form" :rules="loginRules" label-width="80px">
+  <el-form ref="form" :model="form" :rules="loginRules" label-width="80px" class="login-form">
     <el-form-item label="账号">
       <el-input v-model="form.account" placeholder="请输入内容"></el-input>
     </el-form-item>
@@ -7,8 +7,7 @@
       <el-input placeholder="请输入密码" v-model="form.password" show-password></el-input>
     </el-form-item>
     <el-form-item>
-      <el-button type="primary" @click="onSubmit">登录</el-button>
-      <el-button>取消</el-button>
+      <el-button type="primary" style="width: 100%" @click="onSubmit">登录</el-button>
     </el-form-item>
   </el-form>
 </template>
@@ -41,5 +40,12 @@
     }
 </script>
 <style scoped>
-
+  .login-form {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 35px 35px 15px 35px;
+    margin: 120px auto;
+  }
 </style>
