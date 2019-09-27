ebe3f16a388ef0e7e52f838aaf461c0e0fa1834c
zhangqiao <1053373312@qq.com>
2019-09-27 13:26:20 +0800

    修改主页头部组件的名称 取消主页默认页边距 添加logo

---
+++ frontend/src/components/index/PageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div class="left">
+    <img src="../../assets/header_logo.png" alt="">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+.left{
+  float: left;
+}
+</style>
--- frontend/src/views/index/index.vue
+++ frontend/src/views/index/index.vue
@@ -1,29 +1,29 @@
 <template>
-  <el-container>
-    <el-header>
-      <navbar></navbar>
-    </el-header>
     <el-container>
-      <el-aside width="200px">
-        <sidebar></sidebar>
-      </el-aside>
+      <el-header>
+        <page-header></page-header>
+      </el-header>
       <el-container>
-        <el-main>
-          <app-main></app-main>
-        </el-main>
+        <el-aside width="200px">
+          <sidebar></sidebar>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <app-main></app-main>
+          </el-main>
+        </el-container>
       </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <script>
-    import Navbar from '../../components/index/Navbar'
+    import PageHeader from '../../components/index/PageHeader'
     import Sidebar from '../../components/index/Sidebar'
     import AppMain from '../../components/index/AppMain'
     export default {
         name: 'index',
         components: {
-            Navbar,
+            PageHeader,
             Sidebar,
             AppMain
         }
@@ -31,17 +31,22 @@
 </script>
 
 <style lang='less'>
-  .el-container{
-    height: 100vh;
+  body{
+    margin: 0;
+    padding: 0;
+    border: 0;
   }
-  .el-header, .el-footer {
-    background-color: #B3C0D1;
+  .el-container{
+     height: 100vh;
+  }
+  .el-header{
+    background-color: #4868AC;
     color: #333;
     text-align: center;
     line-height: 60px;
   }
   .el-aside {
-    background-color: #D3DCE6;
+    background-color: #304156;
     color: #333;
     text-align: center;
     line-height: 200px;
